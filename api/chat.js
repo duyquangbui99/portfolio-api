dd6854347a044d2873b5df1edf3dca87f4235404
duyquangbui99 <duyquangbui99@gmail.com>
2025-05-06 13:37:57 -0500

    allow all origin

--- api/chat.js
+++ api/chat.js
@@ -6,13 +6,13 @@ const openai = new OpenAI({
 
 module.exports = async (req, res) => {
     if (req.method === 'OPTIONS') {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // or use 'http://localhost:3000'
+        res.setHeader('Access-Control-Allow-Origin', '*'); // or use 'http://localhost:3000'
         res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
         return res.status(200).end();
     }
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // or restrict to your frontend URL
+    res.setHeader('Access-Control-Allow-Origin', '*'); // or restrict to your frontend URL
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     if (req.method !== 'POST') return res.status(405).end('Method Not Allowed');
 
